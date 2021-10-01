649b20a76aa79cdecfe58c607c607a718922c343
sijanneupane <79142189+sijanneupane@users.noreply.github.com>
2021-10-01 08:35:47 +0545

    DAO injected

--- app/src/main/java/com/sijanneupane/runningtracker/MainActivity.kt
+++ app/src/main/java/com/sijanneupane/runningtracker/MainActivity.kt
@@ -3,9 +3,14 @@ package com.sijanneupane.runningtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
-}
